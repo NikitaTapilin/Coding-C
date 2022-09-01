c4b906a660ff2f0ba51d9c3bdfbd6bfe63d629f6
NikitaTapilin <tapilinn@gmail.com>
2022-09-02 00:30:05 +0400

    added examp 5 with if else

---
+++ 1_lecture/examp_5/if_else.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter user name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha"){
+    Console.WriteLine("Ow, it`s Masha!");
+} else {
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}

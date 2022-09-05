a9c1ca998788cb9db41ce79f5639ba5ab4d421b8
NikitaTapilin <tapilinn@gmail.com>
2022-09-05 21:28:32 +0400

    added new folder

---
+++ 1_web/Program.cs
@@ -0,0 +1,8 @@
+﻿//тип данных, название и данные
+int a = 12; //int - целочисленные данные
+float b = 23.4f; //float - дробные числа
+double c = 8.4; // double - дробные числа
+string d = "Hello World"; //string - строковые данные
+bool e = true; //false //bool данные с условием (истина или ложь)
+
+Console.WriteLine(a * 10 + 20 /2 - 500);

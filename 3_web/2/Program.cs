11e3c597637865ad49c0f5514e31d14b7af27ab2
NikitaTapilin <tapilinn@gmail.com>
2022-09-18 18:44:35 +0400

    added 3 web

---
+++ 3_web/2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход
+// число n  и выдаёт таблицу квадратов от 1 до n.
+int number = new Random().Next(1,99);
+int[] array = new int[1];
+for (int i = 1; i < number; i++)
+{
+    int[] newArray = array;
+    array = new int[array.Length +1];
+    for (int j = 0; j < newArray.Length; j++)
+    {
+        array[j] = newArray[j];
+    }
+    if (i * i % 5 ==0)
+    {
+        array[array.Length - 1] = i * i;
+    }
+    //Console.WriteLine(i * i);
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i]);
+}
---
+++ 3_web/3/random_find_max.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int [new Random().Next(1,100)];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100,100);
+}
+Console.WriteLine(string.Join(" ", array));
+
+int max = array[0];
+for (int j = 1; j < array.Length; j++)
+{
+    if (array[j] > max) max = array[j];
+}
+System.Console.WriteLine($"Самое большое число: {max}");

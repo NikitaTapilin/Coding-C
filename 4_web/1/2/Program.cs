e486cc6c5667c15f6af0725a1c04cf34d921e448
NikitaTapilin <tapilinn@gmail.com>
2022-09-19 22:33:45 +0400

    4 web

---
+++ 4_web/1/2/Program.cs
@@ -0,0 +1,27 @@
+﻿ //выводит массив из 8 элементов, заполненного 0 и 1 в случайном порядке
+ //должно быть 2 метода (1 создаёт и заполняет, 2 выводит)
+ 
+ void new_array(int [] array)
+ {
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,2);
+        i ++;
+    }
+ }
+
+ void print_array(int [] array)
+
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(array[position]);
+        position++;
+    }
+}
+
+int [] array = new int [8];
+new_array(array);
+print_array(array);
---
+++ 4_web/1/sum_0-N.cs
@@ -0,0 +1,8 @@
+﻿string a = Console.ReadLine();
+int number = Convert.ToInt32(a);
+int sum = 0;
+for (int i = 1; i <= number; i++)
+{
+    sum += i;
+}
+System.Console.WriteLine(sum);
